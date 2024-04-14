2e3e7db86727052334adb3ba95c61fd17193d496
Sean Craven <sean.craven@advai.co.uk>
2024-04-14 18:38:02 +0100

    chore: update to wezterm.

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,14 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.font_size = 18
+config.color_scheme = 'tokyonight-storm'
+
+-- and finally, return the configuration to wezterm
+return config
