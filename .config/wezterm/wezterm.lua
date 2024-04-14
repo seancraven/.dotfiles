ccc242d74c132d039685205a05dfd33e40db6847
Sean Craven <sean.craven@advai.co.uk>
2024-04-14 18:47:05 +0100

    feat: setup wezterm in stow.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,6 +8,7 @@ local config = wezterm.config_builder()
 
 -- For example, changing the color scheme:
 config.font_size = 18
+config.font = wezterm.font('JetBrains Mono', { weight = "DemiBold" })
 config.color_scheme = 'tokyonight-storm'
 
 -- and finally, return the configuration to wezterm
