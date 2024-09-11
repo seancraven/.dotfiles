92da55f2f2a4cd04d42fc41aa4bb4ab0d6bcc99b
Sean Craven <sean.craven@advai.co.uk>
2024-09-11 23:43:04 +0100

    style: font change

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,10 +7,10 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.font_size = 16
-config.font = wezterm.font("Fira Code", { weight = "DemiBold" })
+config.font_size = 18
+config.font = wezterm.font("Agave Nerd Font", { weight = "DemiBold" })
 config.color_scheme = "tokyonight-storm"
-config.window_background_opacity = 0.6
+config.window_background_opacity = 0.9
 
 -- and finally, return the configuration to wezterm
 return config
